05926b0f0e9d96a9a69f681df12143c5a80e1c5e
MD <manjubd437@gmail.com>
2026-09-15 13:03:10 +0530

    Implement window.onload to initialize resources
    Add window.onload to load public resources on page load

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const API="https://script.google.com/macros/s/AKfycbx2TIiEbBuAkNPZ-6wsyeuwGMb05kwE5HFgH9cdWaYCaMzroaYkU5Vw_IfNDBFaSHuBDA/exec";
 
+window.onload=function(){
+
+loadPublicResources();
+
+}
+
 let role="Student";
 let token="";
 let resources=[];
