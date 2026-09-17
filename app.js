9ba1afc0ac9e05587442a3d665a06b943b1c5cc4
MD <manjubd437@gmail.com>
2026-09-17 14:30:06 +0530

    Refactor localStorage token handling
    Removed redundant localStorage token setting.

--- app.js
+++ app.js
@@ -47,11 +47,7 @@ $("loginForm").onsubmit = async function(e){
     
     localStorage.setItem("user", response.userId);
     localStorage.setItem("token", response.token);
-  localStorage.setItem(
-"token",
-data.token
-);
-
+  
 $("loginView").classList.add("hidden");
 
 $("appView").classList.remove("hidden");
