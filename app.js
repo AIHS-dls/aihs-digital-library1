70f62f16a0e32cc819c190d3b6cc29177549164d
MD <manjubd437@gmail.com>
2026-09-25 12:23:19 +0530

    Fix logical OR condition for box assignment

--- app.js
+++ app.js
@@ -2293,9 +2293,9 @@ async function loadPublicBestUsers(){
 
 
         const box = 
-document.getElementById("studentBestUsers")
-||
 document.getElementById("publicBestUsers");
+||
+document.getElementById("studentBestUsers")
 
 
         if(!box){
